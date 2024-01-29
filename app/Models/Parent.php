93089972a50f86a951019c1a92d8a72df7c5a71d
Natalia <ncalbuq@gmail.com>
2024-01-29 15:54:03 -0300

    create tables, models, migrations

--- app/Models/Parent.php
+++ app/Models/Parent.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Lending extends Model
+class Parent extends Model
 {
     use HasFactory;
 }
