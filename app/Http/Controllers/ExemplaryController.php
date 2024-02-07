ce4074621996dbaf68bce5e4b8f80a6f4a2f9c02
Natalia <ncalbuq@gmail.com>
2024-02-06 22:47:24 -0300

    update ExemplaryController and routes/api.php

--- app/Http/Controllers/ExemplaryController.php
+++ app/Http/Controllers/ExemplaryController.php
@@ -3,8 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\ExemplaryService;
+use App\Http\Requests;
 
 class ExemplaryController extends Controller
 {
-    //
+
+    private ExemplaryService $exemplaryService;
+
+    public function __construct(ExemplaryService $exemplaryService){
+        $this->exemplaryService=$exemplaryService;
+    }
+
+    public function store(ExemplaryRequest $request)
+    {
+ 	  	return $this->exemplaryService->createExemplary($request);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return $this->exemplaryService->showexemplary($id);
+    }
+
+ 
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(exemplaryRequest $request, string $id)
+    {
+		
+     return $this->exemplaryService->updateExemplary($request, $id);
+
+    }
+
+    /**
+     * Remove the specified resource from stoerage.
+     */
+    public function destroy(string $id)
+    {              
+        return $this->exemplaryService->deleteExemplary($id);
+    }
+}
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ExemplaryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,6 +16,7 @@ use App\Http\Controllers\UserController;
 */
 
 Route::resource('users', UserController::class);
+Route::resource('exemplarys', ExemplaryController::class);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
